342efd2eefc21228f1694e0c14b78371214fad3a
Christian Sheen <csheen1725@gmail.com>
2020-03-21 21:09:50 -0400

    finished up styling the flip functionality on the product cards

--- src/components/shop/shop-product.js
+++ src/components/shop/shop-product.js
@@ -7,11 +7,20 @@ class ShopProduct extends Component {
 		const { title, description, price } = this.props;
 		return (
 			<div className="shop-product">
-				<div className="shop-product-title">{title}</div>
-				<div className="shop-product-description">{description}</div>
-				<OrangePriceTag className="shop-product-price" title={price} />
-				<Quantity className="shop-product-quantity" quantity={1} />
-				<div className="shop-product-add-to-cart">Add to cart</div>
+				<div className="shop-product-front">
+					<img
+						className="shop-product-front-image"
+						src="http://via.placeholder.com/220x220"
+					/>
+					<div className="shop-product-front-title">{title}</div>
+				</div>
+				<div className="shop-product-back">
+					<div className="shop-product-back-title">{title}</div>
+					<div className="shop-product-back-description">{description}</div>
+					<OrangePriceTag className="shop-product-back-price" title={price} />
+					<Quantity className="shop-product-back-quantity" quantity={1} />
+					<div className="shop-product-back-add-to-cart">Add to cart</div>
+				</div>
 			</div>
 		);
 	}
